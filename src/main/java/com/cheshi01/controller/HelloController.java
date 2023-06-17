86da92979dade249782443f24bda9817ca71d74e
root <root>
2023-06-17 23:23:26 +0800

    test...

--- src/main/java/com/cheshi01/controller/HelloController.java
+++ src/main/java/com/cheshi01/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("")
     public String hello(){
-        System.out.println("hello world");
-        return "ok";
+        System.out.println("hello world23");
+        return "ok23";
     }
 }
