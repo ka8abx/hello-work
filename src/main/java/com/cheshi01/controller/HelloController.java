cad9082e0b5e84ee2476b659efba574771e3ee7b
root <root>
2023-06-18 14:29:33 +0800

    test...

--- src/main/java/com/cheshi01/controller/HelloController.java
+++ src/main/java/com/cheshi01/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @GetMapping("")
     public String hello(){
         System.out.println("hello world23");
-        return "ok666";
+        return "ok777";
     }
 }
